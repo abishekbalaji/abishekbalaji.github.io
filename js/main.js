6d8b5ff3a10ec0e89c4f8076d91a70fa385ab1fd
Abishek B <abishekbalaji7.151@gmail.com>
2020-09-03 13:18:14 +0530

    Major changes

--- js/main.js
+++ js/main.js
@@ -65,3 +65,29 @@ $(".testimonials-carousel").owlCarousel({
   items: 1,
 });
 
+// client scroll logo//
+$(document).ready(function () {
+  $(".customer-logos").slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+    ],
+  });
+});
